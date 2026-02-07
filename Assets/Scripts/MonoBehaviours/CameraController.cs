20828d9e64af27232bff77e4c702b135f341cb6b
Haro <haro7488@gmail.com>
2026-02-07 16:15:49 +0900

    Cinemachine 기반 카메라 컨트롤러 구현
    - Cinemachine 3.1.5 패키지 추가
    - CameraController로 WASD 이동, QE 회전, 마우스 휠 줌 구현
    - CinemachineCamera + CinemachineBrain 씬 구성

---
+++ Assets/Scripts/MonoBehaviours/CameraController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace DotsRts.MonoBehaviours
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private CinemachineCamera _cinemachineCamera;
+
+        [SerializeField] private float _fieldOfViewMin;
+        [SerializeField] private float _fieldOfViewMax;
+
+        private float _targetFieldOfView;
+
+        private void Awake()
+        {
+            _targetFieldOfView = _cinemachineCamera.Lens.FieldOfView;
+        }
+
+        private void Update()
+        {
+            var moveDir = Vector3.zero;
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                moveDir.z += 1;
+            }
+
+            if (Input.GetKey(KeyCode.S))
+            {
+                moveDir.z -= 1;
+            }
+
+            if (Input.GetKey(KeyCode.A))
+            {
+                moveDir.x -= 1;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                moveDir.x += 1;
+            }
+
+            var mainTransform = Camera.main.transform;
+            moveDir = mainTransform.forward * moveDir.z + mainTransform.right * moveDir.x;
+            moveDir.y = 0f;
+            moveDir.Normalize();
+
+            var moveSpeed = 30f;
+            transform.position += moveSpeed * Time.deltaTime * moveDir;
+
+            var rotationAmount = 0f;
+            if (Input.GetKey(KeyCode.Q))
+            {
+                rotationAmount += 1f;
+            }
+
+            if (Input.GetKey(KeyCode.E))
+            {
+                rotationAmount -= 1f;
+            }
+
+            var rotationSpeed = 200f;
+            transform.eulerAngles += new Vector3(0, rotationAmount * rotationSpeed * Time.deltaTime, 0);
+
+            var zoomAmount = 4f;
+            if (Input.mouseScrollDelta.y > 0)
+            {
+                _targetFieldOfView -= zoomAmount;
+            }
+
+            if (Input.mouseScrollDelta.y < 0)
+            {
+                _targetFieldOfView += zoomAmount;
+            }
+
+            _targetFieldOfView = Mathf.Clamp(_targetFieldOfView, _fieldOfViewMin, _fieldOfViewMax);
+            var zoomSpeed = 10f;
+            _cinemachineCamera.Lens.FieldOfView = Mathf.Lerp(_cinemachineCamera.Lens.FieldOfView,
+                _targetFieldOfView, zoomSpeed * Time.deltaTime);
+        }
+    }
+}
